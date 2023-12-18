fdd4e87a2249a920dd4fa79d8a6ce51b9476afde
foubadev <foubabertrand@gmail.com>
2023-12-18 18:00:55 +0100

    header file

--- main.c
+++ main.c
@@ -1,47 +1,39 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 1024
+global_t global = {NULL, NULL, NULL, 0};
 
 
-
-/**
- * main - principal code interpreter
- * @argc: number of arguments
- * @argv: file position
- * Return: 0 on success
- */
 int main(int argc, char *argv[])
-{   
-	ssize_t read_line = 1;
-	char *content;
-	stack_t *stack = NULL;
-	FILE *file;
-	size_t size = 0;
-	unsigned int line_number = 0;
+{
+    char line[MAX_LINE_LENGTH];
+    stack_t *stack = NULL;
+    FILE *file;
+    unsigned int line_number = 0;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	global.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		global.content = content;
-		line_number++;
-		if (read_line > 0)
-		{
-			compile(content, &stack, line_number, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-	return (0);
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, sizeof(line), file))
+    {
+        line_number++;
+        compile(line, &stack, line_number, file);
+    }
+
+    free_stack(stack);
+    fclose(file);
+
+    return 0;
 }
